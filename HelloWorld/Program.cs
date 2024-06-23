4d890b3498d6b833b51e31472b6097bc8d1781a2
Doplax <doplax@gmail.com>
2024-06-23 14:18:45 +0200

    Exercise: Calculate Area and Perimeter of a Rectangle

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,7 +10,33 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Commit Test in Windows Project");
+            // Variable declaration
+            double height = 0;
+            double width = 0;
+            double perimeter = 0;
+            double area = 0;
+
+
+            //1.Ask for rectangle height
+            Console.Write("height? ");
+            height = Convert.ToDouble(Console.ReadLine());
+
+            // 2.Ask for rectangle width
+            Console.Write("width? ");
+            width = Convert.ToDouble(Console.ReadLine());
+
+            //3.calculate area multiplied by height times width
+            area = height * width;
+
+            //4.calculate the perimeter by adding the four sides together
+            perimeter = 2 * (width + height);
+
+            //5.show area
+            Console.WriteLine("The area is: " + area);
+
+            //6.show perimeter
+            Console.WriteLine("The perimeter is: " + perimeter);
+
 
         }
     }
