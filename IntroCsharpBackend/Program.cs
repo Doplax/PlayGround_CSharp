49d72b349c5a5157235b78e2ad9a12db073961db
Pol Valle <doplax@gmail.com>
2025-02-03 15:54:18 +0100

    feat: Generics

--- IntroCsharpBackend/Program.cs
+++ IntroCsharpBackend/Program.cs
@@ -1,44 +1,75 @@
 ﻿
-var sale = new Sale();
-var beer = new Beer();
+// El número que le pasamos es para el _limit
+var numbers = new MyList<int>(2);
+var names = new MyList<string>(2);
+var beers = new MyList<Beer>(2);
 
 
-//Some está esperando un parametro que implemente la interfaz ISave
-Some(sale);
-Some(beer);
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+numbers.Add(6);
+numbers.Add(7);
+Console.WriteLine(numbers.GetContent());
 
-void Some(ISave save)
+
+names.Add("John");
+names.Add("Doe");
+names.Add("Jane");
+names.Add("Doe");
+names.Add("Jack");
+names.Add("Doe");
+Console.WriteLine(names.GetContent());
+
+
+beers.Add(new Beer { Name = "Corona", Price = 2.5 });
+beers.Add(new Beer { Name = "Heineken", Price = 3.5 });
+beers.Add(new Beer { Name = "Budweiser", Price = 1.5 });
+beers.Add(new Beer { Name = "Modelo", Price = 2.0 });
+Console.WriteLine(beers.GetContent());
+
+public class MyList<T>
 {
-    save.Save();
-}
+    private List<T> _list;
+    private int _limit;
 
-interface ISale
-{
-    decimal Total { get; set; }
-}
-
-interface ISave
-{
-    public void Save();
-}
-
-public class Sale : ISale, ISave
-{
-    public decimal Total { get; set; }
-
-    public void Save()
+    public MyList(int limit)
     {
-        Console.WriteLine("Guardando");
+        _limit = limit;
+        _list = new List<T>();
+    }
+
+    public void Add(T element)
+    {
+        if (_list.Count < _limit)
+        {
+            _list.Add(element);
+        }
+    }
+
+    public string GetContent()
+    {
+        string content = "";
+        foreach (var element in _list)
+        {
+            content += element + ", ";
+        }
+        return content;
+    }
+
+}
+
+
+public class Beer
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+
+    public override string ToString() // Sobreescribimos el método ToString para que nos muestre el nombre y el precio de la cerveza del obejeto Object.
+    {
+        return "Nombre: " + Name + " Price :" + Price;
     }
 }
-
-public class Beer : ISave
-{
-    public void Save()
-    {
-        Console.WriteLine("Guardando");
-    }
-}
-
-
-
