1edd83c906335fe7d575e633e996ac1e6196c88c
Pol Valle <doplax@gmail.com>
2025-02-03 13:05:00 +0100

    feat: create IntroCsharpBackend

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,32 +10,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // Variable declaration
-            double height = 0;
-            double width = 0;
-            double perimeter = 0;
-            double area = 0;
+            
+            int baseNum  = 5;
 
-
-            //1.Ask for rectangle height
-            Console.Write("height? ");
-            height = Convert.ToDouble(Console.ReadLine());
-
-            // 2.Ask for rectangle width
-            Console.Write("width? ");
-            width = Convert.ToDouble(Console.ReadLine());
-
-            //3.calculate area multiplied by height times width
-            area = height * width;
-
-            //4.calculate the perimeter by adding the four sides together
-            perimeter = 2 * (width + height);
-
-            //5.show area
-            Console.WriteLine("The area is: " + area);
-
-            //6.show perimeter
-            Console.WriteLine("The perimeter is: " + perimeter);
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(baseNum * i);
+            }
 
 
         }
