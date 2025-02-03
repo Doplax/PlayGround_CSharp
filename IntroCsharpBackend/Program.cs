35d655611857c68a6983e14eeda68405df7eef11
Pol Valle <doplax@gmail.com>
2025-02-03 14:40:14 +0100

    feat: Herencia

--- IntroCsharpBackend/Program.cs
+++ IntroCsharpBackend/Program.cs
@@ -2,11 +2,34 @@
 //Sale sale = new(); //  Así
 var sale = new Sale(5); // O con var
 
+var salewithTax = new SaleWithTax(5, 1.16m); // Creamos un objeto de la clase hija (La m indica que es un decimal)
 sale.Total = 5; // Si en el contructor quitaramos el set, No podriamos asignarle ningún valor.
 
 
 var message = sale.GetInfo();
 Console.WriteLine(message); // Imprimimos el valor
+
+
+
+class SaleWithTax : Sale
+{
+    public decimal Tax { get; set; }
+    public SaleWithTax(decimal total, decimal tax) : base(total)
+    {
+        Tax = tax;
+        //_someProtected = 5; // Se puede acceder a la propiedad protegida
+    }
+
+    public override string GetInfo() // Con Override sobreescribimos el método virtual del padre
+    {
+        return "El total es" + Total + "Impuesto es: " + Tax;
+    }
+
+    public string GetInfo(int a) // Sobrecarga del método GetInfo
+    {
+        return a.ToString();
+    }
+}
 
 class Sale
 {  
@@ -19,7 +42,7 @@ class Sale
         _some = 8; // Solo se puede acceder a una propiedad privada dentro de la clase
     }
 
-    public string GetInfo()
+    public virtual string GetInfo() // Con virtual le decimos que el método se puede sobreescribir
     {
         return "El toatal es: " + Total;
         //return $"Total: {Total}";
